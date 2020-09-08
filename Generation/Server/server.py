201688f561d9f1c36d0a9619d5c061b2ec095ff5
Jeong-Hyun-Su <mses1572@naver.com>
2020-09-08 18:26:55 +0900

    server.py

--- Generation/Server/server.py
+++ Generation/Server/server.py
@@ -2,10 +2,9 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from flask import Flask, render_template, request
-import Drawing.test_seq_style as transform
+from flask import Flask, render_template, request, Response
+from Drawing import test_seq_style as transform
 from PIL import Image
-
 
 app = Flask(__name__)
 
@@ -16,32 +15,32 @@ def main():
 
 
 @app.route("/transform", methods=['POST'])
-def transform():
+def run():
     try:
         image = request.files['image']
-        if image:
-            # 전달받은 이미지 저장 및 변환
-            image.save("./Drawing/examples/" + image.filename)
-            transform.seq_style()
 
-            # Drawing 폴더에서 생성된 fake 사진의 이름 지
-            img_name = image.filename.split(".")[0] + "_fake.png"
-            # javascript로 보낼 사진 이름 리스트
-            img_lst = dict()
+        # 전달받은 이미지 저장 및 변환
+        image.save("./Drawing/examples/" + image.filename)
+        transform.seq_style()
 
-            # result의 사진들 읽고, Server/static/images로 저장
-            for idx, link in enumerate(['0-0-1/', '0-1-0/', '1-0-0/']):
-                tf_image = Image.open("./Drawing/results/pretrained/test_200/imagesstyle" + link + img_name)
-                tf_dir = "./Server/static/images/" + image.filename.split(".")[0] + "_" + str(idx + 1) + "_fake.png"
-                tf_image.save(tf_dir, 'PNG')
+        # Drawing 폴더에서 생성된 fake 사진의 이름 지
+        img_name = image.filename.split(".")[0] + "_fake.png"
+        # javascript로 보낼 사진 이름 리스트
+        img_lst = dict()
 
-                # ../static/images/이미지 이름
-                tf_dir = ".." + tf_dir[8:]
-                img_lst[idx] = tf_dir
+        # result의 사진들 읽고, Server/static/images로 저장
+        for idx, link in enumerate(['1-0-0/', '0-1-0/', '0-0-1/']):
+            tf_image = Image.open("./Drawing/results/pretrained/test_200/imagesstyle" + link + img_name)
+            tf_dir = "./Server/static/images/" + image.filename.split(".")[0] + "_" + str(idx + 1) + "_fake.png"
+            tf_image.save(tf_dir, 'PNG')
 
-            return img_lst
+            # ../static/images/이미지 이름
+            tf_dir = ".." + tf_dir[8:]
+            img_lst[idx] = tf_dir
 
-    except Exception as e:
+        return img_lst
+
+    except Exception:
         print("error : not contain image")
         return Response("fail", status=400)
 
