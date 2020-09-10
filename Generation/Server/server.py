b467d6e0d4731aa26eb1f8d9142bbe948ee832a0
Jeong-Hyun-Su <42924998+Jeong-Hyun-Su@users.noreply.github.com>
2020-09-10 18:50:39 +0900

    Update server.py

--- Generation/Server/server.py
+++ Generation/Server/server.py
@@ -54,6 +54,7 @@ def main():
 # Sketch Start
 def run(image, style):
     # 전달받은 이미지 저장 및 변환
+    print("run")
     print(os.getcwd())
     image.filename = "input.png"
     file_dir = "./Drawing/examples/" + image.filename
@@ -102,6 +103,9 @@ def sketch():
         print("error : not contain image")
         return Response("fail", status=400)
 
+    print("sketch")
+    print(os.getcwd())
+    
     # Queue - put data
     req = {
         'input': [image, style]
