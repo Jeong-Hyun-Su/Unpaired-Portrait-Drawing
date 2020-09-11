5b04978f3d83aaac8bcee699b346fe7541e2de8f
Jeong-Hyun-Su <mses1572@naver.com>
2020-09-11 10:25:43 +0900

    로커스트

--- Generation/Server/server.py
+++ Generation/Server/server.py
@@ -122,5 +122,6 @@ def healthCheck():
     return "", 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=80)
